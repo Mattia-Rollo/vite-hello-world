7fc1f2260d923ea8343407ad6310da62c2ccaf72
Mattia-Rollo <ste.tia@hotmail.it>
2022-11-09 15:00:25 +0100

    add HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 
 export default {
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderComponent
   },
   data() {
     return{
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Ciao Mondo!!!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            HeaderComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
