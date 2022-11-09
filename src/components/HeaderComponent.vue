3400622e0dc779d70965ee20efc521e0c6db5764
Mattia-Rollo <ste.tia@hotmail.it>
2022-11-09 15:37:45 +0100

    add header with blur filter

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
+    <header>
         <h1>Ciao Mondo!!!</h1>
-    </div>
+        <div class="background-image"></div>
+    </header>
 </template>
 
 <script>
@@ -17,5 +18,36 @@
 </script>
 
 <style scoped>
+    h1 {
+        
+        
+        width: 100%;
+        z-index: 10;
+        margin: 0;
+        padding: 20px;
+        
+    }
+    header{position: fixed;
+        top:0;
+        left:0;
+        width: 100%;
+        z-index: 100;
 
+}
+header .background-image {
+position: absolute;
+top: 0;
+
+width: 100%;
+height: 100%;
+z-index: -1;
+/* -webkit-filter: blur(5px);
+-moz-filter: blur(5px);
+-o-filter: blur(5px);
+-ms-filter: blur(5px); */
+ /* filter: blur(20px); */
+ background-color: rgba(0, 150, 150, 0.3);
+ backdrop-filter: blur(4px);
+
+}
 </style>
